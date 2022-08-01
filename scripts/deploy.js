0193b38665e9e9f7d0b2c599ef6b2a4dda215e26
SmartCoding51 <topstar51@outlook.com>
2022-08-02 04:18:06 +0900

    deploy VeArtProxy & VotingEscrow contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,54 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  /* const Equal = await hre.ethers.getContractFactory("Equal");
+  const equal = await Equal.deploy();
 
-  await greeter.deployed();
+  await equal.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Equal deployed to:", equal.address); */
+
+  /* const PairFactory = await hre.ethers.getContractFactory("PairFactory");
+  const pairFactory = await PairFactory.deploy();
+
+  await pairFactory.deployed();
+
+  console.log("PairFactory deployed to:", pairFactory.address); */
+
+  // Wrapped Fantom address on the testnet
+  /* const WFTM =  "0x07B9c47452C41e8E00f98aC4c075F5c443281d2A";
+  let factory = "0x59aa4da57efd24255f58490c69b81ecfad20d70b";
+
+  const Router = await hre.ethers.getContractFactory("Router");
+  const router = await Router.deploy(factory, WFTM);
+
+  await router.deployed();
+
+  console.log("Router deployed to:", router.address); */
+
+  /* const ROUTER_ADDR = "0xB9f3842dbEd7F90F4B095D0D328B2Ac7C2a88881";
+  const EqualizerLibrary = await hre.ethers.getContractFactory("EqualizerLibrary");
+  const equalizerLibrary = await EqualizerLibrary.deploy(ROUTER_ADDR);
+
+  await equalizerLibrary.deployed();
+
+  console.log("EqualizerLibrary deployed to:", equalizerLibrary.address); */
+
+  /* const VeArtProxy = await hre.ethers.getContractFactory("VeArtProxy");
+  const veArtProxy = await VeArtProxy.deploy();
+
+  await veArtProxy.deployed();
+
+  console.log("VeArtProxy deployed to:", veArtProxy.address); */
+
+  const veArtProxyAddr = "0x3b2eeD935af5E21e3ccB4C090ca60B5d2801ACb5";
+  const equalAddr = "0x9a5FdF8146467d70634fc48bEF67dD14B5A08757";
+  const VotingEscrow = await hre.ethers.getContractFactory("VotingEscrow");
+  const votingEscrow = await VotingEscrow.deploy(equalAddr, veArtProxyAddr);
+
+  await votingEscrow.deployed();
+
+  console.log("VotingEscrow deployed to:", votingEscrow.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
